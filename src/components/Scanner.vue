593e1e93028e55bd6aaeac718c38f9d59efaf8f1
ari000000 <playstation369@outlook.com>
2023-10-13 16:40:43 +0200

    scanner

--- src/components/Scanner.vue
+++ src/components/Scanner.vue
@@ -1,14 +1,42 @@
 <script>
-    export default{
-        data() {
-            return {
-                productId: 1,
-               
-            }
-        }
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+export default {
+  name: "App",
+  components: {
+    StreamBarcodeReader,
+   
+  },
+  data() {
+    return {
+      decodedText: ""
+    };
+  },
+  methods: {
+    onDecode(result) {
+      this.decodedText = result;
+    },
+    onLoaded() {
+      console.log("Scanner loaded and ready!");
+    },
+    onError(error) {
+      console.error("Error while scanning:", error);
     }
+  }
+};
 </script>
 
+
 <template>
-    <h1>Scanner</h1>
-</template>
+    <div id="app">
+      <h1>Barcode & QR Code Scanner</h1>
+  
+      
+      <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+  
+      <div v-if="decodedText">
+        <h2>Decoded Text:</h2>
+        <p>{{ decodedText }}</p>
+      </div>
+    </div>
+  </template>
