9a07dad8e3eab0879cc54ea106188c7eaec20238
= <=>
2023-10-14 16:28:20 +0200

    formating

--- src/components/ProductInformation.vue
+++ src/components/ProductInformation.vue
@@ -23,7 +23,7 @@ import axios from 'axios';
                     this.productArray = response.data
                 })
                 .catch(console.error);
-        },
+            },
         }
     }
 </script>
